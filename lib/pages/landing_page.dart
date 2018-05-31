95a6168fe7ae2951e63f0806738e18df2e64bf37
graphicbeacon <jeropp00@gmail.com>
2018-05-31 21:24:45 +0100

    Creates the landing screen. Sets up launch configuration.

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.greenAccent,
+      child: new InkWell(
+          onTap: () => print("We tapped the page!"),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text("Let's Quizzz",
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold)),
+              new Text("Tap to start",
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold)),
+            ],
+          )),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quiz_app/main.dart';
+import 'package:quiz_app/main_.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
